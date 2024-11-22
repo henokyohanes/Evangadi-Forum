15bf411bb7102e2c99d4e821fdf24ef5312dc5f2
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-21 19:24:03 -0600

    signup component with functionality implemented.

---
+++ Frontend/src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import classes from "./SignUp.module.css";
+import { Link } from "react-router-dom";
+import axiosBaseURL from "../../Utility/ApiConfig";
+import Swal from "sweetalert2";
+
+const Signup = ({ onToggle }) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate form before submitting
+    if (!validateForm()) {
+      return; // Do not submit if there are validation errors
+    }
+
+    try {
+      const response = await axiosBaseURL.post("/users/register", {
+        username: formData.username,
+        firstname: formData.firstName,
+        lastname: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+      });
+
+      if (response.status === 201) {
+        Swal.fire({
+          icon: "success",
+          title: "Registration successful!",
+          text: "You have been registered successfully.",
+        }).then(() => {
+          // Optionally, navigate to another page after success
+          window.location.href = "/";
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Registration failed",
+          text: response.data.msg || "An error occurred during registration.",
+        });
+      }
+    } catch (err) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text:
+          err.response?.data?.msg ||
+          "Error submitting the form. Please try again.",
+      });
+    }
+  };
+
+  return (
+    <div className={classes.signup_container}>
+      <h2>Join the network</h2>
+      <p>
+        Already have an account?{" "}
+        <Link to="/login" onClick={onToggle}>
+          Sign in
+        </Link>
+      </p>
+      
+      {/* Signup Form */}
+      <form onSubmit={handleSubmit}>
+      {/* username */}
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <div className={classes.name_fields}>
+        {/* First Name */}
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First name"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        {/* Last name */}
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last name"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        {/* email */}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email address"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        {/* Password */}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <p>
+          I agree to the <a href="/privacy-policy">privacy policy</a> and{" "}
+          <Link to="/terms">terms of service</Link>.
+        </p>
+        <button type="submit">Agree and Join</button>
+      </form>
+      <p>
+        <Link to="/login" onClick={onToggle}>
+          Already have an account?{" "}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Signup;
