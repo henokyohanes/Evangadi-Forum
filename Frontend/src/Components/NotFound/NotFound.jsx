3c57c989f386a8c780eae6bc513fab4143897a77
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-22 13:38:22 -0600

    lay out and notfound components with responsive styling implemented.

---
+++ Frontend/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import styles from "./NotFound.module.css";
+import Layout from "../Layout/Layout";
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.message}>Oops! Page not found.</p>
+        <a href="/" className={styles.link}>
+          Go back home
+        </a>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
