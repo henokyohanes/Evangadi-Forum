11f2705c1fe37f900e72844717901aab391efafe
henokyohanesdev <henokyohanes8@gmail.com>
2025-03-15 23:47:37 -0500

    footer component position and functionality updated.

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -2,71 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 import logo from "../../assets/Images/logo.png";
-import { PiFacebookLogoLight } from "react-icons/pi";
-import { IoLogoInstagram } from "react-icons/io5";
-import { AiOutlineYoutube } from "react-icons/ai";
-
-const openExternalLink = (url) => {
-  window.open(url, "_blank", "noopener,noreferrer");
-};
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <div className={styles.footerContent}>
-        {/* Logo and Social Media Icons */}
-        <div className={styles.logoAndSocials}>
-          <img src={logo} alt="Evangadi logo" loading="lazy" className={styles.footerLogo} />
-          <div className={styles.socialIcons}>
-            {/* Evangadi Facebook address Link */}
-            <Link
-              onClick={() =>
-                openExternalLink("https://www.facebook.com/evangaditech/")
-              }
-            >
-              <PiFacebookLogoLight />
+      <div className={`${styles.footerContent} row`}>
+        <div className={`${styles.usefulLinks} col-6 col-md-4 ps-sm-5`}>
+          <h3>Useful Link</h3>
+          <p><Link to="/how-it-works">How it works</Link></p>
+          <p><Link to="#">Terms of Service</Link></p>
+          <p><Link to="#">Privacy Policy</Link></p>
+        </div>
+        <div className={`${styles.contactInfo} col-6 col-md-4 ps-sm-5`}>
+          <h3>Contact Info</h3>
+          <p><Link to="#">Evangadi Networks</Link></p>
+          <p><Link to="mailto: support@evangadi.com">support@evangadi.com</Link></p>
+          <p><Link to="tel: +1 202 386 2702">+1 202 386 2702</Link></p>
+        </div>
+        <div className={`${styles.logoAndSocials} col-12 col-md-4`}>
+          <img src={logo} alt="Evangadi logo" loading="lazy" />
+          <div className={styles.socialMedia}>
+            <Link to="https://www.facebook.com/evangaditech/" target="_blank" aria-label="Facebook">
+              <i className="fa-brands fa-facebook-f"></i>
             </Link>
-            {/* Evangadi Instagram Link */}
-            <Link
-              onClick={() =>
-                openExternalLink("https://www.instagram.com/evangaditech/")
-              }
-            >
-              <IoLogoInstagram />
+            <Link to="https://www.instagram.com/evangaditech/" target="_blank" aria-label="Instagram">
+              <i className="fa-brands fa-instagram"></i>
             </Link>
-            {/* Evangadi Channel Youtube Link */}
-            <Link
-              onClick={() =>
-                openExternalLink("https://www.youtube.com/evangaditech")
-              }
-            >
-              <AiOutlineYoutube />
+            <Link to="https://www.x.com/company/evangaditech/" target="_blank" aria-label="Twitter">
+              <i className="fa-brands fa-x-twitter"></i>
+            </Link>
+            <Link to="https://www.youtube.com/evangaditech" target="_blank" aria-label="Youtube">
+              <i className="fa-brands fa-youtube"></i>
             </Link>
           </div>
-        </div>
-
-        {/* Useful Links */}
-        <div className={styles.usefulLinks}>
-          <h4>Useful Link</h4>
-          <ul>
-            <li>
-              <Link to="/how-it-works">How it works</Link>
-            </li>
-            <li>
-              <Link to="/terms-of-service">Terms of Service</Link>
-            </li>
-            <li>
-              <Link to="/privacy-policy">Privacy Policy</Link>
-            </li>
-          </ul>
-        </div>
-
-        {/* Contact Information */}
-        <div className={styles.contactInfo}>
-          <h4>Contact Info</h4>
-          <p>Evangadi Networks</p>
-          <p>support@evangadi.com</p>
-          <p>+1-202-386-2702</p>
         </div>
       </div>
     </footer>
