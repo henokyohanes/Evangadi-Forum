0d29cf239d31a5cb8691be02f8f0f9138b7dda2b
henokyohanesdev <henokyohanes8@gmail.com>
2025-03-16 20:03:41 -0500

    footer component functionalities with fully responsive styling implemented.

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./Footer.module.css";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppState } from "../../Routes/Router";
 import logo from "../../assets/Images/logo.png";
+import styles from "./Footer.module.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer = () => {
+  const { isLoggedIn } = useContext(AppState);
+  const navigate = useNavigate();
+
+  // Navigate based on the user's login status when logo is clicked
+  const handleLogoClick = () => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/auth");
+    }
+  };
+
   return (
     <footer className={styles.footerContainer}>
       <div className={`${styles.footerContent} row`}>
-        <div className={`${styles.usefulLinks} col-6 col-md-4 ps-sm-5`}>
+        <div className={`${styles.usefulLinks} col-6 col-md-4`}>
           <h3>Useful Link</h3>
           <p><Link to="/how-it-works">How it works</Link></p>
           <p><Link to="#">Terms of Service</Link></p>
@@ -20,8 +33,11 @@ const Footer = () => {
           <p><Link to="mailto: support@evangadi.com">support@evangadi.com</Link></p>
           <p><Link to="tel: +1 202 386 2702">+1 202 386 2702</Link></p>
         </div>
+        <div className={`${styles.divider} d-md-none`}></div>
         <div className={`${styles.logoAndSocials} col-12 col-md-4`}>
-          <img src={logo} alt="Evangadi logo" loading="lazy" />
+          <div onClick={handleLogoClick}>
+            <img src={logo} alt="Evangadi logo" loading="lazy" />
+          </div>
           <div className={styles.socialMedia}>
             <Link to="https://www.facebook.com/evangaditech/" target="_blank" aria-label="Facebook">
               <i className="fa-brands fa-facebook-f"></i>
