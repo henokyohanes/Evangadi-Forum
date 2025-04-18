bd32b3de10838656c285edfebe57e85dd67c0f45
henokyohanesdev <henokyohanes8@gmail.com>
2025-04-17 22:25:16 -0500

    forgot password page functionality with fully responsive styling implemented.

--- Frontend/src/pages/ForgotPassword/ForgotPassword.jsx
+++ Frontend/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,22 +1,47 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ScaleLoader } from "react-spinners";
 import axiosBaseURL from "../../Utility/axios";
 import Swal from "sweetalert2";
+import NotFound from "../../Components/NotFound/NotFound";
 import Layout from "../../Components/Layout/Layout";
 import styles from "./ForgotPassword.module.css";
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState("");
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [formErrors, setFormErrors] = useState({});
+    const navigate = useNavigate();
+
+    const validateForm = () => {
+        let isValid = true;
+        const input = {};
+
+        //email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email) {
+            input.Errors = "Email is required.";
+            isValid = false;
+        } else if (!emailRegex.test(email)) {
+            input.Errors = "Invalid email format.";
+            isValid = false;
+        }
+
+        setFormErrors(input);
+        return isValid;
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!email) return;
+        if (!validateForm()) return;
 
         setLoading(true);
+        setError(false);
 
         try {
             const response = await axiosBaseURL.post("/users/forgot-password", {
-              email,
+                email,
             });
 
             if (response.status === 200) {
@@ -24,14 +49,35 @@ const ForgotPassword = () => {
                     title: "Email Sent",
                     text: "Check your inbox for password reset instructions.",
                     icon: "success",
+                    customClass: {
+                        popup: styles.popup,
+                        confirmButton: styles.confirmButton,
+                        icon: styles.icon,
+                        title: styles.successTitle,
+                        htmlContainer: styles.text,
+                    },
+                }).then(() => {
+                    navigate("/");
                 });
             }
         } catch (error) {
-            Swal.fire({
-                title: "Error",
-                text: error?.response?.data?.msg || "Something went wrong.",
-                icon: "error",
-            });
+            console.error("Error sending password reset email:", error);
+            if (error?.response) {
+                Swal.fire({
+                    title: "Error",
+                    text: error?.response?.data?.msg || "Something went wrong. Please try again later.",
+                    icon: "error",
+                    customClass: {
+                        popup: styles.popup,
+                        confirmButton: styles.confirmButton,
+                        icon: styles.icon,
+                        title: styles.errorTitle,
+                        htmlContainer: styles.text,
+                    }
+                });
+            } else {
+                setError(true);
+            }
         } finally {
             setLoading(false);
         }
@@ -39,24 +85,27 @@ const ForgotPassword = () => {
 
     return (
         <Layout>
-            <div className={styles.forgotContainer}>
-                <h2>Reset Your Password</h2>
-                <p>
-                    Please enter the email address associated with your account. We’ll
-                    send you a link to reset your password and regain access.
-                </p>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="email"
-                        placeholder="Enter your email address"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <button type="submit" disabled={loading}>
-                        {loading ? "Sending..." : "Send Reset Link"}
-                    </button>
-                </form>
-            </div>
+            {!error ? (<div className={styles.forgotPasswordContainer}>
+                <div className={styles.forgotPassword}>
+                    <h2>Reset Your Password</h2>
+                    <p>
+                        Please enter the email address associated with your account. Then We’ll
+                        send you a link to reset your password and regain access.
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                        {formErrors && <div className={styles.formError}>{formErrors.Errors}</div>}
+                        <input
+                            type="email"
+                            placeholder="Enter your email address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <button type="submit" disabled={loading}>
+                            {loading ? <ScaleLoader color="#fff" height={12} /> : "Send Reset Link"}
+                        </button>
+                    </form>
+                </div>
+            </div>) : <NotFound />}
         </Layout>
     );
 };
